11751d2834fdafa1822adfb7ad438bb36682663d
mily77 <773258709@qq.com>
2018-04-30 18:05:01 +0800

    city router

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
       <span class="iconfont">&#xe632;</span>
       输入城市/景点/游玩注题
       </div>
-    <div class="header-right">
-      {{this.city}}
-      <span class="iconfont arrow-icon">&#xe6aa;</span>
+    <router-link to="/city">
+      <div class="header-right">
+        {{this.city}}
+        <span class="iconfont arrow-icon">&#xe6aa;</span>
       </div>
+    </router-link>
   </div>
 </template>
 
@@ -27,7 +29,7 @@ export default {
   @import '~styles/varibles.styl';
   .header
     display : flex
-    line-height : .86rem
+    line-height : $headerHeight
     background : $bgColor
     color : #fff
     .header-left
@@ -50,6 +52,7 @@ export default {
       width : 1.24rem
       float right
       text-align : center
+      color : #ffffff
       .arrow-icon
         margin-left : -.10rem
         font-size : .30rem
